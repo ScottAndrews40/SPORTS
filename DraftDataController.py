975cc884cbff78a532d855d26fbde8535c764cfb
Eldon Hayes <eldon.hayes.eh@gmail.com>
2020-06-24 11:21:56 -0700

    Update DraftDataController.py
    Test Change from Github Desktop

--- DraftDataController.py
+++ DraftDataController.py
@@ -1,1 +1,4 @@
-#Manipluates data from model
+# Manipluates data from model and sends to view to be displayed
+# Will have a database separate from the excel files created with scraped
+# information.
+# Database will most likely be created using SQLite python library
