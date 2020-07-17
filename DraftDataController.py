9714f89473e06fa70c0ead9d6f6b40a1d841c23d
Redflag40 <scott.andrews4088@gmail.com>
2020-07-16 17:34:24 -0700

    Pandas>Excel
    Transfered raw list data to pandas data frames, there are some issues with the raw model data that needs to be corrected.  Changed list header decleration in Model(Combined soups) file.

--- DraftDataController.py
+++ DraftDataController.py
@@ -2,3 +2,28 @@
 # Will have a database separate from the excel files created with scraped
 # information.
 # Database will most likely be created using SQLite python library
+
+import pandas as pd
+import sqlite3 as sq
+import combined_soups as model_data
+
+# Position lists for Model to be inserted to EXCEL
+# HEADER_LIST = model_data.HEADER_LIST
+# print(model_data.QB_LIST)
+data_frameQB = pd.DataFrame(model_data.QB_LIST)
+data_frameRB = pd.DataFrame(model_data.RB_LIST)
+data_frameWR = pd.DataFrame(model_data.WR_LIST)
+data_frameTE = pd.DataFrame(model_data.TE_LIST)
+data_frameK = pd.DataFrame(model_data.K_LIST)
+data_frameDEF = pd.DataFrame(model_data.D_LIST)
+
+# print(data_frameWR)
+
+with pd.ExcelWriter('model_data.xlsx') as writer:
+    data_frameQB.to_excel(writer, sheet_name='QB')
+    data_frameRB.to_excel(writer, sheet_name='RB')
+    data_frameWR.to_excel(writer, sheet_name='WR')
+    data_frameTE.to_excel(writer, sheet_name='TE')
+    data_frameK.to_excel(writer, sheet_name='K')
+    data_frameDEF.to_excel(writer, sheet_name='DEF')
+    writer.save()
--- combined_soups.py
+++ combined_soups.py
@@ -11,7 +11,7 @@ QB_HTML = req.get(QB_URL)
 QB_SOUP = BeautifulSoup(QB_HTML.content, 'html.parser')
 QB_RANK_WINDOW = QB_SOUP.find(id='rank-data')
 QB_DATA = QB_RANK_WINDOW.find_all('tr')
-QB_LIST = []
+# QB_LIST = []
 
 # BeautifulSoup setup for scraping of running backs and their stats
 RB_URL = 'https://www.fantasypros.com/nfl/rankings/half-point-ppr-rb-cheatsheets.php'
@@ -19,7 +19,7 @@ RB_HTML = req.get(RB_URL)
 RB_SOUP = BeautifulSoup(RB_HTML.content, 'html.parser')
 RB_RANK_WINDOW = RB_SOUP.find(id='rank-data')
 RB_DATA = RB_RANK_WINDOW.find_all('tr')
-RB_LIST = []
+# RB_LIST = []
 
 # BeautifulSoup setup for scraping of wide receivers and their stats
 WR_URL = 'https://www.fantasypros.com/nfl/rankings/half-point-ppr-wr-cheatsheets.php'
@@ -27,50 +27,58 @@ WR_HTML = req.get(WR_URL)
 WR_SOUP = BeautifulSoup(WR_HTML.content, 'html.parser')
 WR_RANK_WINDOW = WR_SOUP.find(id='rank-data')
 WR_DATA = WR_RANK_WINDOW.find_all('tr')
-WR_LIST = []
+# WR_LIST = []
 
 # BeautifulSoup setup for scraping of kickers and their stats
 K_URL = 'https://www.fantasypros.com/nfl/rankings/k-cheatsheets.php'
 K_HTML = req.get(K_URL)
 K_SOUP = BeautifulSoup(K_HTML.content, 'html.parser')
-K_RANK_WINDOW = K_SOUP.find(id = "rank-data")
+K_RANK_WINDOW = K_SOUP.find(id="rank-data")
 K_DATA = K_RANK_WINDOW.find_all('tr')
-K_LIST = []
+# K_LIST = []
 
 # BeautifulSoup setup for scraping of tight ends and their stats
 TE_URL = 'https://www.fantasypros.com/nfl/rankings/half-point-ppr-te-cheatsheets.php'
 TE_HTML = req.get(TE_URL)
 TE_SOUP = BeautifulSoup(TE_HTML.content, 'html.parser')
-TE_RANK_WINDOW = TE_SOUP.find(id = "rank-data")
+TE_RANK_WINDOW = TE_SOUP.find(id="rank-data")
 TE_DATA = TE_RANK_WINDOW.find_all('tr')
-TE_LIST = []
+# TE_LIST = []
 
 # BeautifulSoup setup for scraping of defenses and their stats
 DEF_URL = 'https://www.fantasypros.com/nfl/rankings/dst-cheatsheets.php'
 DEF_HTML = req.get(DEF_URL)
 DEF_SOUP = BeautifulSoup(DEF_HTML.content, 'html.parser')
-DEF_RANK_WINDOW = DEF_SOUP.find(id = "rank-data")
+DEF_RANK_WINDOW = DEF_SOUP.find(id="rank-data")
 DEF_DATA = DEF_RANK_WINDOW.find_all('tr')
-DEF_LIST = []
+# DEF_LIST = []
 
 # Header List for first entry in defense/kicker/tight end lists
 HEADER_LIST = ["Rank", "First Name", "Last Name", "Team", "BYE", "BEST",
                "WORST", "AVG", "STP DEV", "ADP", "VS. ADP"]
 
+
 # Base function for scraping
 def scrape_position(pos_data) -> list:
-    position_list = []
-    position_list.append(HEADER_LIST)
+    position_list = [HEADER_LIST]
 
     for players in pos_data:
         if players.has_attr('data-id'):
             position_list.append(players.text.split())
-    
+
     for elem in position_list:
         if (elem[2]) != 'Last Name':
             del elem[2]
 
     return position_list
+
+
+QB_LIST = scrape_position(QB_DATA)
+RB_LIST = scrape_position(RB_DATA)
+WR_LIST = scrape_position(WR_DATA)
+K_LIST = scrape_position(K_DATA)
+TE_LIST = scrape_position(TE_DATA)
+D_LIST = scrape_position(DEF_DATA)
 
 if __name__ == '__main__':
     QB_LIST = scrape_position(QB_DATA)
